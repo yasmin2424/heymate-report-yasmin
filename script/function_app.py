78758918de4ed59833fb99b2cc88ae5ebb54038c
Hankun Xiao <xh1319@student.ubc.ca>
2025-05-23 16:09:53 -0700

    clean up code

--- script/function_app.py
+++ script/function_app.py
@@ -1,11 +1,5 @@
-# import sys
-# import os
-# sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
-
 import azure.functions as func
 import logging
-# from script.main_task import main
-# from script.write_log import write_log
 
 from main_task import main
 from write_log import write_log
